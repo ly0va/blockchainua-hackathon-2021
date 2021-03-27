4b1c621ec0e53bdd0636e482aa93c8a6d3c075db
Vladyslav-Bochok <vladbochok1@gmail.com>
2021-03-27 18:01:13 +0200

    Add ERC20 token

--- whitelisting-contract/contracts/token.sol
+++ whitelisting-contract/contracts/token.sol
@@ -1,19 +1,268 @@
-// SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// SPDX-License-Identifier: UNLICENSED
 
-contract ERC20Token is ERC20 {
-    constructor(
-        string memory name,
-        string memory symbol
-    ) ERC20(name, symbol) {
-        
+import "./SafeMath.sol";
+
+contract ERC20Token {
+    /// @dev EIP-20 token name for this token
+    string public constant name = "Zkam";
+
+    /// @dev EIP-20 token symbol for this token
+    string public constant symbol = "ZKM";
+
+    /// @dev EIP-20 token decimals for this token
+    uint8 public constant decimals = 18;
+
+    /// @dev Total number of tokens in circulation
+    uint public totalSupply = 1_000_000_000e18;
+
+    /// @dev Address which may mint new tokens
+    address public minter;
+
+    /// @dev The timestamp after which minting may occur
+    uint public mintingAllowedAfter;
+
+    /// @dev Minimum time between mints
+    uint32 public constant minimumTimeBetweenMints = 1 days * 365;
+
+    /// @dev Cap on the percentage of totalSupply that can be minted at each mint
+    uint8 public constant mintCap = 2;
+
+    /// @dev Allowance amounts on behalf of others
+    mapping (address => mapping (address => uint256)) internal allowances;
+
+    /// @dev Official record of token balances for each account
+    mapping (address => uint256) internal balances;
+
+    /// @dev A record of each accounts delegate
+    mapping (address => address) public delegates;
+
+    /// @dev A checkpoint for marking number of votes from a given block
+    struct Checkpoint {
+        uint32 fromBlock;
+        uint256 votes;
     }
 
-    function mint(address _to, uint256 _amount) public returns (bool) {
-        _mint(_to, _amount);
+    /// @dev A record of votes checkpoints for each account, by index
+    mapping (address => mapping (uint32 => Checkpoint)) public checkpoints;
+
+    /// @dev The number of checkpoints for each account
+    mapping (address => uint32) public numCheckpoints;
+
+    /// @dev A record of states for signing / validating signatures
+    mapping (address => uint) public nonces;
+
+    /// @dev An event thats emitted when the minter address is changed
+    event MinterChanged(address minter, address newMinter);
+
+    /// @dev An event thats emitted when an account changes its delegate
+    event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate);
+
+    /// @dev An event thats emitted when a delegate account's vote balance changes
+    event DelegateVotesChanged(address indexed delegate, uint previousBalance, uint newBalance);
+
+    /// @dev The standard EIP-20 transfer event
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    /// @dev The standard EIP-20 approval event
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+
+    /**
+     * @dev Construct a new ZKM token
+     * @param account The initial account to grant all the tokens
+     * @param minter_ The account with minting ability
+     * @param mintingAllowedAfter_ The timestamp after which minting may occur
+     */
+    constructor(address account, address minter_, uint mintingAllowedAfter_) public {
+        require(mintingAllowedAfter_ >= block.timestamp, "ZKM::constructor: minting can only begin after deployment");
+
+        balances[account] = totalSupply;
+        emit Transfer(address(0), account, totalSupply);
+        minter = minter_;
+        emit MinterChanged(address(0), minter);
+        mintingAllowedAfter = mintingAllowedAfter_;
+    }
+
+    /**
+     * @dev Change the minter address
+     * @param minter_ The address of the new minter
+     */
+    function setMinter(address minter_) external {
+        require(msg.sender == minter, "ZKM::setMinter: only the minter can change the minter address");
+        emit MinterChanged(minter, minter_);
+        minter = minter_;
+    }
+
+    /**
+     * @dev Mint new tokens
+     * @param dst The address of the destination account
+     * @param amount The number of tokens to be minted
+     */
+    function mint(address dst, uint amount) external {
+        require(msg.sender == minter, "ZKM::mint: only the minter can mint");
+        require(block.timestamp >= mintingAllowedAfter, "ZKM::mint: minting not allowed yet");
+        require(dst != address(0), "ZKM::mint: cannot transfer to the zero address");
+
+        // record the mint
+        mintingAllowedAfter = SafeMath.add(block.timestamp, minimumTimeBetweenMints);
+
+        require(amount <= SafeMath.div(SafeMath.mul(totalSupply, mintCap), 100), "ZKM::mint: exceeded mint cap");
+        totalSupply = SafeMath.add(totalSupply, amount);
+
+        // transfer the amount to the recipient
+        balances[dst] = SafeMath.add(balances[dst], amount);
+        emit Transfer(address(0), dst, amount);
+
+        // move delegates
+        _moveDelegates(address(0), delegates[dst], amount);
+    }
+
+    /**
+     * @dev Get the number of tokens `spender` is approved to spend on behalf of `account`
+     * @param account The address of the account holding the funds
+     * @param spender The address of the account spending the funds
+     * @return The number of tokens approved
+     */
+    function allowance(address account, address spender) external view returns (uint) {
+        return allowances[account][spender];
+    }
+
+    /**
+     * @dev Approve `spender` to transfer up to `amount` from `src`
+     * @dev This will overwrite the approval amount for `spender`
+     *  and is subject to issues noted [here](https://eips.ethereum.org/EIPS/eip-20#approve)
+     * @param spender The address of the account which may transfer tokens
+     * @param amount The number of tokens that are approved
+     * @return Whether or not the approval succeeded
+     */
+    function approve(address spender, uint amount) external returns (bool) {
+        allowances[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
         return true;
+    }
+
+    /**
+     * @dev Get the number of tokens held by the `account`
+     * @param account The address of the account to get the balance of
+     * @return The number of tokens held
+     */
+    function balanceOf(address account) external view returns (uint) {
+        return balances[account];
+    }
+
+    /**
+     * @dev Transfer `amount` tokens from `msg.sender` to `dst`
+     * @param dst The address of the destination account
+     * @param amount The number of tokens to transfer
+     * @return Whether or not the transfer succeeded
+     */
+    function transfer(address dst, uint amount) external returns (bool) {
+        _transferTokens(msg.sender, dst, amount);
+        return true;
+    }
+
+    /**
+     * @dev Transfer `amount` tokens from `src` to `dst`
+     * @param src The address of the source account
+     * @param dst The address of the destination account
+     * @param amount The number of tokens to transfer
+     * @return Whether or not the transfer succeeded
+     */
+    function transferFrom(address src, address dst, uint amount) external returns (bool) {
+        address spender = msg.sender;
+        uint256 spenderAllowance = allowances[src][spender];
+
+        if (spender != src) {
+            uint256 newAllowance = SafeMath.sub(spenderAllowance, amount);
+            allowances[src][spender] = newAllowance;
+
+            emit Approval(src, spender, newAllowance);
+        }
+
+        _transferTokens(src, dst, amount);
+        return true;
+    }
+
+    /**
+     * @dev Delegate votes from `msg.sender` to `delegatee`
+     * @param delegatee The address to delegate votes to
+     */
+    function delegate(address delegatee) public {
+        return _delegate(msg.sender, delegatee);
+    }
+
+    /**
+     * @dev Gets the current votes balance for `account`
+     * @param account The address to get votes balance
+     * @return The number of current votes for `account`
+     */
+    function getCurrentVotes(address account) external view returns (uint256) {
+        uint32 nCheckpoints = numCheckpoints[account];
+        return nCheckpoints > 0 ? checkpoints[account][nCheckpoints - 1].votes : 0;
+    }
+
+    function _delegate(address delegator, address delegatee) internal {
+        address currentDelegate = delegates[delegator];
+        uint256 delegatorBalance = balances[delegator];
+        delegates[delegator] = delegatee;
+
+        emit DelegateChanged(delegator, currentDelegate, delegatee);
+
+        _moveDelegates(currentDelegate, delegatee, delegatorBalance);
+    }
+
+    function _transferTokens(address src, address dst, uint256 amount) internal {
+        require(src != address(0), "ZKM::_transferTokens: cannot transfer from the zero address");
+        require(dst != address(0), "ZKM::_transferTokens: cannot transfer to the zero address");
+
+        balances[src] = SafeMath.sub(balances[src], amount);
+        balances[dst] = SafeMath.add(balances[dst], amount);
+        emit Transfer(src, dst, amount);
+
+        _moveDelegates(delegates[src], delegates[dst], amount);
+    }
+
+    function _moveDelegates(address srcRep, address dstRep, uint256 amount) internal {
+        if (srcRep != dstRep && amount > 0) {
+            if (srcRep != address(0)) {
+                uint32 srcRepNum = numCheckpoints[srcRep];
+                uint256 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
+                uint256 srcRepNew = SafeMath.sub(srcRepOld, amount);
+                _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
+            }
+
+            if (dstRep != address(0)) {
+                uint32 dstRepNum = numCheckpoints[dstRep];
+                uint256 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
+                uint256 dstRepNew = SafeMath.add(dstRepOld, amount);
+                _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
+            }
+        }
+    }
+
+    function _writeCheckpoint(address delegatee, uint32 nCheckpoints, uint256 oldVotes, uint256 newVotes) internal {
+      uint32 blockNumber = safe32(block.number, "ZKM::_writeCheckpoint: block number exceeds 32 bits");
+
+      if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
+          checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
+      } else {
+          checkpoints[delegatee][nCheckpoints] = Checkpoint(blockNumber, newVotes);
+          numCheckpoints[delegatee] = nCheckpoints + 1;
+      }
+
+      emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
+    }
+
+    function safe32(uint n, string memory errorMessage) internal pure returns (uint32) {
+        require(n < 2**32, errorMessage);
+        return uint32(n);
+    }
+
+    function getChainId() internal view returns (uint) {
+        uint256 chainId;
+        assembly { chainId := chainid() }
+        return chainId;
     }
 }
